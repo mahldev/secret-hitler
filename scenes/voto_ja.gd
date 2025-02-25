6d7fdf65d9bc67164864d28abf96acf34493ee3f
Simborrio <117034964+Simborrio@users.noreply.github.com>
2025-02-25 20:54:31 +0100

    agrego escenas realizadas en casitat uwu

---
+++ scenes/voto_ja.gd
@@ -0,0 +1,11 @@
+extends Button
+
+@export var card_name: String = "voto-ja"
+
+func _ready():
+	# Conectar la señal de pulsación
+	self.pressed.connect(_on_card_pressed)
+
+func _on_card_pressed():
+	# Muestra una alerta con el nombre de la carta
+	print("Has pulsado la carta: " + card_name)
